844e4ec86fa303d9b5765410c8880e20a9af9710
dapoi <daffaprabowo5@gmail.com>
2025-07-13 22:07:27 +0700

    update: add coffee donation vector and update related strings in the donation dialog

--- feature/home/src/main/java/com/dapascript/mever/feature/home/screen/component/HandleDonationDialogOffer.kt
+++ feature/home/src/main/java/com/dapascript/mever/feature/home/screen/component/HandleDonationDialogOffer.kt
@@ -1,13 +1,20 @@
 package com.dapascript.mever.feature.home.screen.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale.Companion.Crop
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign.Companion.Center
 import com.dapascript.mever.core.common.R
 import com.dapascript.mever.core.common.ui.attr.MeverDialogAttr.MeverDialogArgs
 import com.dapascript.mever.core.common.ui.component.MeverDialog
+import com.dapascript.mever.core.common.ui.theme.Dimens.Dp200
 import com.dapascript.mever.core.common.ui.theme.MeverTheme.typography
 
 @Composable
@@ -19,13 +26,21 @@ internal fun HandleDonationDialogOffer(
     MeverDialog(
         showDialog = showDialog,
         meverDialogArgs = MeverDialogArgs(
-            title = stringResource(R.string.buy_coffee),
+            title = stringResource(R.string.hi_there),
             primaryButtonText = stringResource(R.string.yes),
             secondaryButtonText = stringResource(R.string.later),
             onClickPrimaryButton = onClickPrimaryButton,
             onClickSecondaryButton = onClickSecondaryButton
         )
     ) {
+        Image(
+            modifier = Modifier
+                .size(Dp200)
+                .align(CenterHorizontally),
+            painter = painterResource(R.drawable.ic_coffee),
+            contentScale = Crop,
+            contentDescription = "Error Image"
+        )
         Text(
             text = stringResource(R.string.donation_desc),
             textAlign = Center,
