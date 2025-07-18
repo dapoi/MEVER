314443d23a9e07e973be9e147c684d08b6426f75
dapoi <daffaprabowo5@gmail.com>
2025-07-18 19:54:20 +0700

    update: enhance TiktokDownloaderResponse to support multiple media types and improve entity mapping

--- core/data/src/main/java/com/dapascript/mever/core/data/model/remote/TiktokDownloaderResponse.kt
+++ core/data/src/main/java/com/dapascript/mever/core/data/model/remote/TiktokDownloaderResponse.kt
@@ -1,16 +1,46 @@
 package com.dapascript.mever.core.data.model.remote
 
 import com.dapascript.mever.core.data.model.local.ContentEntity
+import com.google.gson.JsonElement
 import com.google.gson.annotations.SerializedName
 
 data class TiktokDownloaderResponse(
     @SerializedName("data") val data: DataContent
 ) {
     data class DataContent(
-        @SerializedName("video") val video: String? = null
-    )
+        @SerializedName("photo") val rawPhoto: JsonElement?,
+        @SerializedName("video") val rawVideo: JsonElement?
+    ) {
+        val photos: List<String>?
+            get() = if (rawPhoto?.isJsonArray == true) {
+                rawPhoto.asJsonArray.mapNotNull {
+                    if (it.isJsonPrimitive && it.asJsonPrimitive.isString) it.asString else null
+                }
+            } else null
+        val video: String?
+            get() = if (rawVideo?.isJsonPrimitive == true && rawVideo.asJsonPrimitive.isString) {
+                rawVideo.asString
+            } else null
+    }
 
-    fun mapToEntity() = listOf(
-        ContentEntity(url = data.video.orEmpty())
-    )
+    fun mapToEntity(): List<ContentEntity> {
+        val contentList = mutableListOf<ContentEntity>()
+        data.photos?.forEach { photoUrl ->
+            contentList.add(
+                ContentEntity(
+                    url = photoUrl,
+                    type = "jpg"
+                )
+            )
+        }
+        data.video?.let { videoUrl ->
+            contentList.add(
+                ContentEntity(
+                    url = videoUrl,
+                    type = "mp4"
+                )
+            )
+        }
+        return contentList
+    }
 }
