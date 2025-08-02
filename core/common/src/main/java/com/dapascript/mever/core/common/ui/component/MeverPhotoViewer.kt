5a9904d6c9950411bcd0ecb71c7f87aa4e7afb81
dapoi <daffaprabowo5@gmail.com>
2025-08-02 20:27:06 +0700

    update: remove unused import for tween animation in MeverPhotoViewer

--- core/common/src/main/java/com/dapascript/mever/core/common/ui/component/MeverPhotoViewer.kt
+++ core/common/src/main/java/com/dapascript/mever/core/common/ui/component/MeverPhotoViewer.kt
@@ -1,7 +1,6 @@
 package com.dapascript.mever.core.common.ui.component
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
