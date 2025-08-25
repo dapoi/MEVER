364bcb4d49c7e5c03b3446aa5ed4abbb83ff2f5e
dapoi <daffaprabowo5@gmail.com>
2025-08-25 22:25:27 +0700

    update: rename unchecked circle icon and enhance MeverRadioButton styling

--- core/common/src/main/java/com/dapascript/mever/core/common/ui/component/MeverRadioButton.kt
+++ core/common/src/main/java/com/dapascript/mever/core/common/ui/component/MeverRadioButton.kt
@@ -1,10 +1,12 @@
 package com.dapascript.mever.core.common.ui.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement.spacedBy
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
@@ -17,9 +19,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import com.dapascript.mever.core.common.R
+import com.dapascript.mever.core.common.ui.theme.Dimens.Dp14
 import com.dapascript.mever.core.common.ui.theme.Dimens.Dp16
 import com.dapascript.mever.core.common.ui.theme.Dimens.Dp24
+import com.dapascript.mever.core.common.ui.theme.Dimens.Dp3
 import com.dapascript.mever.core.common.ui.theme.Dimens.Dp8
+import com.dapascript.mever.core.common.ui.theme.MeverPurple
 import com.dapascript.mever.core.common.ui.theme.MeverTheme.typography
 import com.dapascript.mever.core.common.util.onCustomClick
 
@@ -39,16 +44,21 @@ fun MeverRadioButton(
     horizontalArrangement = spacedBy(Dp16)
 ) {
     Box(
-        modifier = modifier.clip(CircleShape).onCustomClick { onValueChoose() },
+        modifier = modifier
+            .clip(CircleShape)
+            .onCustomClick { onValueChoose() },
         contentAlignment = Center
     ) {
+        if (isChoosen) Box(
+            modifier = Modifier
+                .background(MeverPurple, CircleShape)
+                .size(Dp14)
+                .padding(Dp3)
+        )
         Icon(
-            painter = painterResource(
-                if (isChoosen) R.drawable.ic_circle_checked
-                else R.drawable.ic_circle_unchecked
-            ),
+            painter = painterResource(R.drawable.ic_circle),
             contentDescription = "Radio button",
-            tint = colorScheme.primary
+            tint = MeverPurple
         )
     }
     Text(
