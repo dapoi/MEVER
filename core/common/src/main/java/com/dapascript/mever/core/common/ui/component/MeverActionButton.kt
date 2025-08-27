4be28f94ac6de485ec1846817b8757d8cdf55d78
dapoi <daffaprabowo5@gmail.com>
2025-08-27 23:43:26 +0700

    update: refactor MeverActionButton to remove IconButton and implement custom click handling

--- core/common/src/main/java/com/dapascript/mever/core/common/ui/component/MeverActionButton.kt
+++ core/common/src/main/java/com/dapascript/mever/core/common/ui/component/MeverActionButton.kt
@@ -10,15 +10,13 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Badge
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableLongStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.TopEnd
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -29,6 +27,8 @@ import com.dapascript.mever.core.common.ui.theme.Dimens.Dp15
 import com.dapascript.mever.core.common.ui.theme.Dimens.Dp2
 import com.dapascript.mever.core.common.ui.theme.Dimens.Dp24
 import com.dapascript.mever.core.common.ui.theme.Dimens.Dp32
+import com.dapascript.mever.core.common.ui.theme.Dimens.Dp8
+import com.dapascript.mever.core.common.util.onCustomClick
 
 @Composable
 fun MeverActionButton(
@@ -50,16 +50,20 @@ fun MeverActionButton(
         label = "Rotation Animation"
     )
 
-    Box(modifier = modifier.size(Dp32)) {
-        IconButton(onClick = onDebounceClick { onClick() }) {
-            Icon(
-                modifier = Modifier
-                    .size(Dp24)
-                    .showGraphicLayer(state = showBadge, value = animateIconVibrate),
-                painter = painterResource(id = resource),
-                contentDescription = "Action Button"
-            )
-        }
+    Box(
+        modifier = modifier
+            .size(Dp32)
+            .clip(RoundedCornerShape(Dp8))
+            .onCustomClick { onClick() }
+    ) {
+        Icon(
+            modifier = Modifier
+                .size(Dp24)
+                .showGraphicLayer(state = showBadge, value = animateIconVibrate)
+                .align(Center),
+            painter = painterResource(id = resource),
+            contentDescription = "Action Button"
+        )
         if (showBadge) Badge(
             modifier = Modifier
                 .size(Dp15)
@@ -80,19 +84,3 @@ private fun Modifier.showGraphicLayer(state: Boolean, value: Float) = if (state)
         rotationZ = value
     )
 } else this
-
-@Composable
-private fun onDebounceClick(
-    debounceTimeMillis: Long = 1000L,
-    onClick: () -> Unit
-): () -> Unit {
-    var lastClickTimeMillis: Long by remember { mutableLongStateOf(value = 0L) }
-    return {
-        System.currentTimeMillis().let { currentTimeMillis ->
-            if ((currentTimeMillis - lastClickTimeMillis) >= debounceTimeMillis) {
-                lastClickTimeMillis = currentTimeMillis
-                onClick()
-            }
-        }
-    }
-}
