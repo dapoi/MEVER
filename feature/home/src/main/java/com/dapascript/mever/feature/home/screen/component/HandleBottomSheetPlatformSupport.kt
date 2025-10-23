75ec6b8d2bc31bcc415db6b49b40699c20e8c3ac
dapoi <daffaprabowo5@gmail.com>
2025-10-24 00:43:40 +0700

    update: simplify platform filtering in HandleBottomSheetPlatformSupport and remove unused UI components

--- feature/home/src/main/java/com/dapascript/mever/feature/home/screen/component/HandleBottomSheetPlatformSupport.kt
+++ feature/home/src/main/java/com/dapascript/mever/feature/home/screen/component/HandleBottomSheetPlatformSupport.kt
@@ -1,24 +1,18 @@
 package com.dapascript.mever.feature.home.screen.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -32,21 +26,17 @@ import com.dapascript.mever.core.common.ui.theme.Dimens.Dp16
 import com.dapascript.mever.core.common.ui.theme.Dimens.Dp4
 import com.dapascript.mever.core.common.ui.theme.Dimens.Dp48
 import com.dapascript.mever.core.common.ui.theme.Dimens.Dp8
-import com.dapascript.mever.core.common.ui.theme.MeverLightViolet
-import com.dapascript.mever.core.common.ui.theme.MeverPurple
 import com.dapascript.mever.core.common.ui.theme.MeverTheme.typography
 import com.dapascript.mever.core.common.ui.theme.TextDimens.Sp18
 import com.dapascript.mever.core.common.util.PlatformType
 import com.dapascript.mever.core.common.util.PlatformType.AI
 import com.dapascript.mever.core.common.util.PlatformType.ALL
 import com.dapascript.mever.core.common.util.PlatformType.EXPLORE
-import com.dapascript.mever.core.common.util.PlatformType.YOUTUBE
-import com.dapascript.mever.core.common.util.PlatformType.YOUTUBE_MUSIC
 import com.dapascript.mever.core.common.util.onCustomClick
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HandleBottomSheetPlatformSupport(
+internal fun HandleBottomSheetPlatformSupport(
     showPlatformSupportDialog: Boolean,
     modifier: Modifier = Modifier,
     onDismiss: () -> Unit
@@ -84,7 +74,7 @@ fun HandleBottomSheetPlatformSupport(
         ) {
             Spacer(modifier = Modifier.size(Dp4))
             val platforms = PlatformType.entries
-                .filterNot { it in listOf(AI, ALL, EXPLORE, YOUTUBE, YOUTUBE_MUSIC) }
+                .filterNot { it in listOf(AI, ALL, EXPLORE) }
                 .map { it.platformName }
 
             platforms.forEachIndexed { index, platform ->
@@ -95,36 +85,7 @@ fun HandleBottomSheetPlatformSupport(
                     platformName = platform
                 )
 
-                if (index < platforms.size - 1) {
-                    HorizontalDivider()
-                }
-            }
-            HorizontalDivider()
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(Dp16),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Box(
-                    modifier = Modifier
-                        .background(
-                            color = MeverLightViolet,
-                            shape = CircleShape
-                        )
-                        .size(Dp48),
-                    contentAlignment = Center
-                ) {
-                    Text(
-                        text = "+2",
-                        textAlign = TextAlign.Center,
-                        style = typography.bodyBold1,
-                        color = MeverPurple
-                    )
-                }
-                Text(
-                    text = stringResource(R.string.more),
-                    style = typography.body1,
-                    color = colorScheme.onPrimary
-                )
+                if (index < platforms.size - 1) HorizontalDivider()
             }
             Spacer(modifier = Modifier.size(Dp4))
         }
