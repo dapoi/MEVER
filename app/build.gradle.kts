2d73c7fe0df04e2b39f9a7ef4f2487302aac5320
dapoi <daffaprabowo5@gmail.com>
2025-12-24 22:00:52 +0700

    update: increment version code to 20251231

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.dapascript.mever"
-        versionCode = 20251225
+        versionCode = 20251231
         versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
