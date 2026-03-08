e75f63a7db7b43ea31113bd7b509b5befd3e797f
dapoi <daffaprabowo5@gmail.com>
2026-03-08 13:44:26 +0700

    fix: remove unused WorkManager initialization in MainApp

--- app/src/main/java/com/dapascript/mever/MainApp.kt
+++ app/src/main/java/com/dapascript/mever/MainApp.kt
@@ -3,7 +3,6 @@ package com.dapascript.mever
 import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
-import androidx.work.WorkManager
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import javax.inject.Inject
@@ -17,8 +16,6 @@ open class MainApp : Application(), Configuration.Provider {
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
-
-        WorkManager.initialize(this, workManagerConfiguration)
     }
 
     override fun getWorkManagerConfiguration(): Configuration {
